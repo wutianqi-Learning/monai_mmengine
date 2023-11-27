04ca6a1e5b87f47e689e9f98648d0aeae63e7e5b
wutianqi-Learning <2675704567@qq.com>
2023-11-27 17:14:21 +0800

    monai unet

---
+++ configs/monai_uent/unet_100e_sgd_brats21_96x96x96_dual_branch.py
@@ -0,0 +1,66 @@
+from mmengine.config import read_base
+from monai.losses.dice import DiceCELoss, DiceLoss, DiceFocalLoss
+from monai.networks.nets import UNet
+from seg.models.segmentors.monai_model import MonaiSeg
+from monai.losses.focal_loss import FocalLoss
+from seg.models.segmentors.monai_dual_model import MonaiDualSeg, MSELoss
+from seg.models.decode_heads.dual_branch import DualBranchRes, DualBranchTanh
+from seg.engine.hooks.set_epoch_hook import SetEpochInfoHook
+from seg.models.monai_datapreprocessor import MonaiBratsPreProcessor
+
+with read_base():
+    from .._base_.datasets.brats21 import *  # noqa
+    from .._base_.schedules.schedule_100e_sgd import *  # noqa
+    from .._base_.monai_runtime import *  # noqa
+
+# model settings
+model = dict(
+    type=MonaiDualSeg,
+    num_classes=4,
+    roi_shapes=roi,
+    backbone=dict(
+        type=UNet,
+        spatial_dims=3,
+        in_channels=4,
+        out_channels=3,
+        kernel_size=5,
+        channels=(8, 16, 32, 64, 128),
+        strides=(2, 2, 2, 2),
+        num_res_units=2),
+    # decoder=dict(
+    #     type=DualBranchTanh,
+    #     # spatial_dims=3,
+    #     # out_channels=1
+    # ),
+    loss_functions=[
+        dict(type=DiceLoss, to_onehot_y=False, sigmoid=True, squared_pred=True, include_background=True),
+        dict(type=MSELoss, steady_point=50)
+        ],
+    # data_preprocessor=dict(type=MonaiBratsPreProcessor),
+    infer_cfg=dict(
+        inf_size=roi,
+        sw_batch_size=4,    # number of sliding window batch size
+        infer_overlap=0.5   # sliding window inference overlap
+    ))
+# custom hooks
+custom_hooks = [dict(type=SetEpochInfoHook)]
+default_hooks.update(
+    dict(
+        logger=dict(interval=10, val_interval=50)))
+
+val_cfg = dict(type=MonaiValLoop, print_log_per_case=False)
+test_cfg = dict(type=MonaiTestLoop, print_log_per_case=False)
+
+vis_backends = [
+    dict(type=LocalVisBackend),
+    dict(
+        type=WandbVisBackend,
+        init_kwargs=dict(
+            project='brats21', name='unet-tiny-sgd-100e'),
+        define_metric_cfg=dict(Dice='max'))
+]
+visualizer = dict(type=SegLocalVisualizer,
+                  vis_backends=vis_backends,
+                  name='visualizer')
+
+work_dir = '../working_brats21/SGD_100epochs/unet_num_res_2/resuent-daul_branch-monai-DiceLoss-MSE'
---
+++ configs/monai_uent/unetmod_tiny_d8_200e_sgd_brats19_96x96x96.py
@@ -0,0 +1,57 @@
+from mmengine.config import read_base
+from monai.losses.dice import DiceCELoss, DiceLoss, DiceFocalLoss
+from monai.networks.nets import UNet
+from seg.models.segmentors.monai_model import MonaiSeg
+from monai.losses.focal_loss import FocalLoss
+from seg.models.segmentors.monai_dual_model import MonaiDualSeg, MSELoss
+from seg.models.decode_heads.dual_branch import DualBranchRes, DualBranchTanh
+from seg.engine.hooks.set_epoch_hook import SetEpochInfoHook
+from seg.models.monai_datapreprocessor import MonaiBratsPreProcessor
+
+with read_base():
+    from .._base_.datasets.brats19 import *  # noqa
+    from .._base_.schedules.schedule_200e_sgd import *  # noqa
+    from .._base_.monai_runtime import *  # noqa
+
+# model settings
+model = dict(
+    type=MonaiSeg,
+    num_classes=4,
+    roi_shapes=roi,
+    backbone=dict(
+        type=UNet,
+        spatial_dims=3,
+        in_channels=1,
+        out_channels=1,
+        kernel_size=5,
+        channels=(8, 16, 32, 64, 128),
+        strides=(2, 2, 2, 2),
+        num_res_units=2),
+    loss_functions=
+        dict(type=DiceCELoss, to_onehot_y=False, sigmoid=False, squared_pred=True, include_background=True),
+    data_preprocessor=dict(type=MonaiBratsPreProcessor),
+    infer_cfg=dict(
+        inf_size=roi,
+        sw_batch_size=4,    # number of sliding window batch size
+        infer_overlap=0.5   # sliding window inference overlap
+    ))
+default_hooks.update(
+    dict(
+        logger=dict(interval=10, val_interval=50)))
+
+val_cfg = dict(type=MonaiValLoop, print_log_per_case=False)
+test_cfg = dict(type=MonaiTestLoop, print_log_per_case=False)
+
+vis_backends = [
+    dict(type=LocalVisBackend),
+    dict(
+        type=WandbVisBackend,
+        init_kwargs=dict(
+            project='brats19', name='unet-tiny-sgd-200e'),
+        define_metric_cfg=dict(Dice='max'))
+]
+visualizer = dict(type=SegLocalVisualizer,
+                  vis_backends=vis_backends,
+                  name='visualizer')
+
+work_dir = '../working_brats19/SGD_200epochs/unet_num_res_2/resuent-monai-DiceCELoss'
